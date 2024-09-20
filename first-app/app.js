91fb8b0ba0b6c28f523cdc70ef26ad0ed9a44bf9
Muawaz Saleem <muawazsaleem@gmail.com>
2024-09-20 12:27:18 +0500

    Extending EventEmitter

--- first-app/app.js
+++ first-app/app.js
@@ -1,15 +1,13 @@
 
 const log = require('./logger')
-const EventEmitter = require('events')
-const emitter = new EventEmitter()
+
+const Logger = require('./logger')
+const logger = new Logger()
 
 // Regiser a listner
-emitter.on('messageLogged', (arg) => {
+logger.on('messageLogged', (arg) => {
     console.log('Listner called', arg);
     
 })
 
-// Raise an event
-emitter.emit('messageLogged', {id: 1, url: 'http://'})
-
-log('this is the message')
+logger.log('message')
--- first-app/logger.js
+++ first-app/logger.js
@@ -1,20 +1,17 @@
 
 const EventEmitter = require('events')
-const emitter = new EventEmitter();
 
-// Register a Listening
-emitter.on('loggin', (arg) => {
-    console.log('listener called: ', arg);
-    
-})
 var url = 'http://mylogger.io/log'
 
-const log = (message) => {
-    //  Send an http request
-    console.log(message);
+class Logger extends EventEmitter {
+    log = (message) => {
+        //  Send an http request
+        console.log(message);
+    
+        // Raise an event
+        this.emit('messageLogged', {id: 1, url: 'http://'})
+    }
 
-    // Raise an event
-    emitter.emit('loggin', {data: message})
 }
 
-module.exports = log
+module.exports = Logger
