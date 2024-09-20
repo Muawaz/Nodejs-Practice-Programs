6f3a3c9f78dbec570604f8016c4953298146b9b8
Muawaz Saleem <muawazsaleem@gmail.com>
2024-09-20 11:46:03 +0500

    Module Wrapper Function

--- first-app/logger.js
+++ first-app/logger.js
@@ -1,11 +1,19 @@
-var url = 'http://mylogger.io/log'
+// (funtion (exports, require, module, __filename, __dirname) {
+    console.log(__filename)
+    console.log(__dirname)
+    
+    var url = 'http://mylogger.io/log'
+    
+    function log(message) {
+        //  Send an http request
+        console.log(message);
+    }
+    
+    module.exports = log
 
-function log(message) {
-    //  Send an http request
-    console.log(message);
-}
+    // module.exports.log = log;
+    // exports.log = log;
 
-// module.exports.log = log
-// module.exports.endPoint = url
+    // exports = log // error
 
-module.exports = log
+// }) 
