8f9a9e076ed528cf66ef4dffe22a492f8f364c57
Muawaz Saleem <muawazsaleem@gmail.com>
2024-09-20 12:21:45 +0500

    EventEmitter

--- first-app/app.js
+++ first-app/app.js
@@ -1,12 +1,15 @@
 
+const log = require('./logger')
 const EventEmitter = require('events')
 const emitter = new EventEmitter()
 
 // Regiser a listner
-emitter.on('messageLogged', function(){
-    console.log('Listner called');
+emitter.on('messageLogged', (arg) => {
+    console.log('Listner called', arg);
     
 })
 
 // Raise an event
-emitter.emit('messageLogged')
+emitter.emit('messageLogged', {id: 1, url: 'http://'})
+
+log('this is the message')
--- first-app/logger.js
+++ first-app/logger.js
@@ -1,19 +1,20 @@
-// (funtion (exports, require, module, __filename, __dirname) {
-    console.log(__filename)
-    console.log(__dirname)
-    
-    var url = 'http://mylogger.io/log'
-    
-    function log(message) {
-        //  Send an http request
-        console.log(message);
-    }
-    
-    module.exports = log
 
-    // module.exports.log = log;
-    // exports.log = log;
+const EventEmitter = require('events')
+const emitter = new EventEmitter();
 
-    // exports = log // error
+// Register a Listening
+emitter.on('loggin', (arg) => {
+    console.log('listener called: ', arg);
+    
+})
+var url = 'http://mylogger.io/log'
 
-// }) 
+const log = (message) => {
+    //  Send an http request
+    console.log(message);
+
+    // Raise an event
+    emitter.emit('loggin', {data: message})
+}
+
+module.exports = log
