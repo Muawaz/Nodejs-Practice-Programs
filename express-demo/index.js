d4f677b5fec197e4fdc8db76b35ce99cc4caf11f
Muawaz Saleem <muawazsaleem@gmail.com>
2024-09-27 15:24:11 +0500

    Route Parameters

--- express-demo/index.js
+++ express-demo/index.js
@@ -10,8 +10,20 @@ app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3, 4, 5, 6]);
 });
 
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id)
+});
+
+app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.params)
+});
+
+app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.query)
+});
+
 // PORT
 const port = process.env.PORT || 3000;
 // Windows PowerShell : $env:PORT=5000
 
-app.listen(3000, () => console.log(`listening on http://localhost:${port}`));
+app.listen(port, () => console.log(`listening on http://localhost:${port}`));
