0e6c9d90cd271c6dcac72092055d2b808bcd904b
Muawaz Saleem <muawazsaleem@gmail.com>
2024-10-10 14:43:00 +0500

    Configuration

--- 3. express-demo/index.js
+++ 3. express-demo/index.js
@@ -1,15 +1,22 @@
+const config = require('config')
+const helmet = require('helmet')
+const morgan = require('morgan')
 const Joi = require('joi');
 const logger = require('./logger')
 const authenticator = require('./authenticator')
 const express = require('express');
-const helmet = require('helmet')
-const morgan = require('morgan')
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(helmet())
+
+// Configuration
+console.log('Applicaiton Name : ' + config.get('name'));
+console.log('Mail Server : ' + config.get('mail.host'));
+console.log('Mail Password : ' + config.get('mail.password'));
+
 
 if (app.get('env') === 'development') {
     app.use(morgan('tiny'));
